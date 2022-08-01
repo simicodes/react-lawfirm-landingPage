d160691d9d0496d160470bedb3bddd4604c0e88c
simicodes <simeonoffiong@gmail.com>
2022-08-01 16:38:45 +0100

    Testimonials and TestimonialSlider updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // import components
 import Banner from "./components/Banner";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
     <div>
       <Banner />
+      <Testimonials />
       <div className="h-[2000px]"></div>
     </div>
   );
