7d43a2c642374a995281a5a30a51dfa0ba0295b2
simicodes <simeonoffiong@gmail.com>
2022-08-01 15:22:33 +0100

    Banner (HERO SECTION) component updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 // import components
-import Header from "./components/Header";
 import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <div className="h-[2000px]">
-        <Banner />
-      </div>
+      <Banner />
+      <div className="h-[2000px]"></div>
     </div>
   );
 };
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,42 @@
 import React from "react";
 
+//import components
+import Header from "./Header";
+import Appointment from "./Appointment";
+
+//import lawyer image
+import LawyerImage from "../assets/img/lawyer_banner.png";
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section
+      id="home"
+      className="lg:h-full lg:max-h-800 bg-gradient-to-t from-[#f8f3ec] to-[#e0c8ac]"
+    >
+      <Header />
+      <div className="container mx-auto h-full lg:flex pt-32">
+        {/* left side */}
+        <div className="text-center h-full lg:text-left px-8 md:p-0">
+          <h1 className="font-primary font-black text-6xl lg:text-7xl text-primary mb-4 lg:mt-[70px] leading-tight lg:leading-[90px]">
+            Your Problem <br />
+            <span className="text-accent">Our Goal.</span>
+          </h1>
+          <p className="max-w-sm mx-auto mb-[50px] lg:mx-0 lg:max-w-[540px]">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </p>
+          {/* Appointment */}
+          <div className="lg:absolute mx-auto max-w-[445px] lg:mx-0">
+            <Appointment />
+          </div>
+        </div>
+        {/* Right side */}
+        <div className="hidden flex-1 lg:flex lg:flex-col items-end">
+          <img src={LawyerImage} alt="/" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
