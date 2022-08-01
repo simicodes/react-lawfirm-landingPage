b07ba914a745e2a6eed8ec1783edfab92d5dbadf
simicodes <simeonoffiong@gmail.com>
2022-08-01 17:30:07 +0100

    Skills component updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // import components
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Banner />
       <Testimonials />
+      <Skills />
       <div className="h-[2000px]"></div>
     </div>
   );
