d633272869cd263ac750e6fd86df4c1fdd08cb28
simicodes <simeonoffiong@gmail.com>
2022-08-02 09:07:18 +0100

    Team component updated

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,9 +14,26 @@ const Team = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl
           eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a
           bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis
-          tincidunt laoreet
+          tincidunt laoreet.
         </p>
         {/* team members grid */}
+        <div className="lg:grid lg:grid-cols-3 lg:gap-x-[30px]">
+          {team.map((item, index) => {
+            const { image, name, position, description } = item;
+            return (
+              <div className="text-center lg:text-left mb-12" key={index}>
+                <img className="mx-auto lg:mx-0 mb-6" src={image} alt="/" />
+                <h4 className="text-2xl mb-2 font-primary font-bold">{name}</h4>
+                <p className="text-sm uppercase tracking-[0.3px] mb-4 opacity-[0.8]">
+                  {position}
+                </p>
+                <p className="max-w-[332px] lg:max-w-[335px] mx-auto lg:mx-0">
+                  {description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
